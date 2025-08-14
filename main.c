e6293d16ca12d283f68e50391eef40a9bd909b2c
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-14 11:32:50 -0300

    Alteração do main
    Coloquei as bibliotecas, criei a função main e dentro da função coloquei os printf para a tela principal do sistema.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("---------------------------------" "\n");
+    printf("     BEM VINDO AO SIG-HOTEL!     " "\n");
+    printf("---------------------------------" "\n");
+
+    system("pause");
+    return(0);
+}
