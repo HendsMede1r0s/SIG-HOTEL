528827a98959249cfb712f2b95ec56ff1ed5cd76
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-14 20:56:04 -0300

    Update main.c
    Adicionei as funções do projeto.

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+
+void tela_inicial(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
 
 int main(void){
-    printf("---------------------------------" "\n");
-    printf("     BEM VINDO AO SIG-HOTEL!     " "\n");
-    printf("---------------------------------" "\n");
+    
+}
 
-    system("pause");
-    return(0);
+void tela_sobre(void){
+    setLocale(LC_ALL,"Portuguese");
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("----------------------------------------------------------");
+    printf("--                                                      --");
+    printf("--      UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE     --");
+    printf("--          CENTRO DE ENSINO SUPERIOR DO SERIDO         --");
+    printf("--        DEPATARMENTO DE COMPUTAÇÃO E TECNOLOGIA       --");
+    printf("--           DISCIPLINA DCT1106 - PROGRAMAÇÃO           --");
+    printf("--   PROJETO SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS   --");
+    printf("--                                                      --");
+    printf("----------------------------------------------------------");
+    print("\n");
+    printf("         SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS         ");
+    printf("Projeto proposto pelo professor de Sistemas de Informação da UFRN-CERES, @flgorgonio, com o intuido fazer um sistema de gestão designado a hotéis ou pousadas");
+    printf("----------------------------------------------------------");
+    printf("\n");
+    printf("\t\t PRESSIONE ENTER PARA CONTINUAR...");
+
 }
