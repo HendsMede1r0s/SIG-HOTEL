b1c44442cdfc19d3807f7b83ca5ce0eac0f7e5a7
Henderson <hendersonbernardo2018@gmail.com>
2025-08-14 22:46:07 -0300

    modulos
    criei e adicionei módulos na tela principal

--- main.c
+++ main.c
@@ -1,20 +1,60 @@
+//////////////////////////////////////
+/////////// Bibliotecas //////////////
+//////////////////////////////////////
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
 
+
+//////////////////////////////////////
+////////////// Funções ///////////////
+//////////////////////////////////////
+
+
 void tela_inicial(void);
-void tela_sobre(void);
 void tela_equipe(void);
+void tela_sobre(void);
+
+
+//////////////////////////////////////
+///////////// Módulos ////////////////
+//////////////////////////////////////
 
 
 int main(void){
-    
+    setlocale(LC_ALL,"Portuguese");
 }
 
+
+void tela_inicial(void){
+    printf("___________________________________");
+    printf("|                                  |");
+    printf("|          Menu Principal          |");
+    printf("|__________________________________|");
+    printf("|                                  |");
+    printf("|        1 - Clientes              |");
+    printf("|        2 - Funcionários          |");
+    printf("|        3 - Quartos               |");
+    printf("|        4 - Serviços              |");
+    printf("|        5 - Restaurante           |");
+    printf("|        6 - Áreas de Lazer        |");
+    printf("|        7 - Garagem               |");
+    printf("|        0 - Sair                  |");
+    printf("|__________________________________|");
+
+
+}
+
+
+void tela_equipe(void){
+
+}
+
+
 void tela_sobre(void){
-    setLocale(LC_ALL,"Portuguese");
-    char op;
     system("clear||cls");
     printf("\n");
     printf("----------------------------------------------------------");
@@ -31,5 +71,5 @@ void tela_sobre(void){
     printf("Projeto proposto pelo professor de Sistemas de Informação da UFRN-CERES, @flgorgonio, com o intuido fazer um sistema de gestão designado a hotéis ou pousadas");
     printf("----------------------------------------------------------");
     printf("\n");
-    printf("\t\t PRESSIONE ENTER PARA CONTINUAR");
+    printf("\t\t PRESSIONE ENTER PARA CONTINUAR...");
 }
