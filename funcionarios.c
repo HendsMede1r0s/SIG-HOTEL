8145cffd63846e74b7767521ab9779d0778f6735
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-09-12 16:58:41 +0000

    Add biblioteca funcionarios

---
+++ funcionarios.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "funcionarios.h"
+
+int tela_funcionarios(){
+    system("clear||cls");
+    int op_funcionarios;
+    printf("\n");
+    printf("___________________________________\n");
+    printf("|                                  |\n");
+    printf("|          Funcionarios            |\n");
+    printf("|__________________________________|\n");
+    printf("|                                  |\n");
+    printf("|        1 - Cadastar              |\n");
+    printf("|        2 - Editar informacoes    |\n");
+    printf("|        3 - Ver Funcionario       |\n");
+    printf("|        0 - Voltar                |\n");
+    printf("|__________________________________|\n");
+    printf("\n");
+    printf("PRESSIONE ENTER PARA CONTINUAR...");
+    printf("Digite uma opção: ");
+    scanf("%d", &op_funcionarios);
+    getchar();
+    return op_funcionarios;
+}
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include "clientes.h"
+#include "funcionarios.h"
 #include "quartos.h"
 #include "servicos.h"
 #include "restaurante.h"
@@ -230,29 +231,6 @@ int tela_inicial(void){
     scanf("%d",&opcao);
     return opcao;
     getchar();  
-}
-
-
-int tela_funcionarios(){
-    system("clear||cls");
-    int op_funcionarios;
-    printf("\n");
-    printf("___________________________________\n");
-    printf("|                                  |\n");
-    printf("|          Funcionarios            |\n");
-    printf("|__________________________________|\n");
-    printf("|                                  |\n");
-    printf("|        1 - Cadastar              |\n");
-    printf("|        2 - Editar informacoes    |\n");
-    printf("|        3 - Ver Funcionario       |\n");
-    printf("|        0 - Voltar                |\n");
-    printf("|__________________________________|\n");
-    printf("\n");
-    printf("PRESSIONE ENTER PARA CONTINUAR...");
-    printf("Digite uma opção: ");
-    scanf("%d", &op_funcionarios);
-    getchar();
-    return op_funcionarios;
 }
 
 
