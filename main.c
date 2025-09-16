c7de48c3c899f6113420c43e2ff18e56f8ddda10
HendsMede1r0s <hendersonbernardo2018@gmail.com>
2025-09-15 23:34:54 -0300

    modularização_sobre

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include "servicos.h"
 #include "restaurante.h"
 #include "garagem.h"
+#include "sobre.h"
 
 
 
@@ -150,29 +151,6 @@ int main(void){
 ///////////// Módulos ////////////////
 //////////////////////////////////////
 
-
-
-
-void tela_sobre(void){
-    system("clear||cls");
-    printf("\n");
-    printf(" ____________________________________________________________________\n");
-    printf("|                                                                    |\n");
-    printf("|             UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE            |\n");
-    printf("|                 CENTRO DE ENSINO SUPERIOR DO SERIDO                |\n");
-    printf("|               DEPATARMENTO DE COMPUTAÇÃO E TECNOLOGIA              |\n");
-    printf("|                  DISCIPLINA DCT1106 - PROGRAMAÇÃO                  |\n");
-    printf("|          PROJETO SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS          |\n");
-    printf("|--------------------------------------------------------------------|\n");
-    printf("|                                                                    |\n");
-    printf("|           - SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS -             |\n");
-    printf("|     Projeto proposto pelo professor de Sistemas de Informação      |\n");
-    printf("|   da UFRN-CERES, @flgorgonio, com o intuido fazer um sistema de    |\n");
-    printf("|              gestão designado a hotéis ou pousadas                 |\n");
-    printf("|____________________________________________________________________|\n");
-    printf("\n");
-    getchar();
-}
 
 void tela_equipe(void){
     system("clear||cls");
---
+++ sobre.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sobre.h"
+
+
+void tela_sobre(void){
+    system("clear||cls");
+    printf("\n");
+    printf(" ____________________________________________________________________\n");
+    printf("|                                                                    |\n");
+    printf("|             UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE            |\n");
+    printf("|                 CENTRO DE ENSINO SUPERIOR DO SERIDO                |\n");
+    printf("|               DEPATARMENTO DE COMPUTAÇÃO E TECNOLOGIA              |\n");
+    printf("|                  DISCIPLINA DCT1106 - PROGRAMAÇÃO                  |\n");
+    printf("|          PROJETO SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS          |\n");
+    printf("|--------------------------------------------------------------------|\n");
+    printf("|                                                                    |\n");
+    printf("|           - SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS -             |\n");
+    printf("|     Projeto proposto pelo professor de Sistemas de Informação      |\n");
+    printf("|   da UFRN-CERES, @flgorgonio, com o intuido fazer um sistema de    |\n");
+    printf("|              gestão designado a hotéis ou pousadas                 |\n");
+    printf("|____________________________________________________________________|\n");
+    printf("\n");
+    getchar();
+}
