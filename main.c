988db355b313512a349fbdd64a23f9b76aa01f29
HendsMede1r0s <hendersonbernardo2018@gmail.com>
2025-09-15 23:41:23 -0300

    modularização_equipe

--- main.c
+++ main.c
@@ -152,34 +152,6 @@ int main(void){
 //////////////////////////////////////
 
 
-void tela_equipe(void){
-    system("clear||cls");
-    printf("\n");
-    printf(" _____________________________________________________________________\n");
-    printf("|                                                                     |\n");
-    printf("|             UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE             |\n");
-    printf("|                 CENTRO DE ENSINO SUPERIO DO SERIDÓ                  |\n");
-    printf("|               DEPARTAMENTO DE COMPUTAÇÃO E TECNOLOGIA               |\n");
-    printf("|                  DISCIPLINA DCT1106 - PROGRAMAÇÃO                   |\n");
-    printf("|          PROJETO SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS           |\n");
-    printf("|---------------------------------------------------------------------|\n");
-    printf("|                                                                     |\n");
-    printf("|           - SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS -              |\n");
-    printf("|                                                                     |\n");
-    printf("|                      PROJETO DESENVOLVIDO POR:                      |\n");
-    printf("|                                                                     |\n");
-    printf("|        NOME:   Henderson Emanuel                                    |\n");
-    printf("|        EMAIL:  hendersonbernardo2018@gmail.com                      |\n");
-    printf("|        GITHUB: https://github.com/HendsMede1r0s                     |\n");
-    printf("|                                                                     |\n");
-    printf("|        NOME:   Leonardo Relva                                       |\n");
-    printf("|        EMAIL:  leonardo.relva.111@ufrn.edu.br                       |\n");
-    printf("|        GITHUB: https://github.com/leonardorelva-ufrn                |\n");
-    printf("|_____________________________________________________________________|\n");
-    printf("\n");
-    getchar();
-}
-
 
 void tela_final(void){
     system("clear||cls");
