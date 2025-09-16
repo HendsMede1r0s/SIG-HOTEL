6a64a62a79d02703495f3f29f92d6db067755d79
HendsMede1r0s <hendersonbernardo2018@gmail.com>
2025-09-15 23:25:20 -0300

    passando_menu_para_a_pasta

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include "menu_inicial.h"
 #include "clientes.h"
 #include "funcionarios.h"
 #include "quartos.h"
@@ -150,32 +151,6 @@ int main(void){
 //////////////////////////////////////
 
 
-
-int tela_inicial(void){
-    system("clear||cls");
-    int opcao;
-    printf("\n");
-    printf(" __________________________________\n");
-    printf("|                                  |\n");
-    printf("|          Menu Principal          |\n");
-    printf("|__________________________________|\n");
-    printf("|                                  |\n");
-    printf("|        1 - Clientes              |\n");
-    printf("|        2 - Funcionarios          |\n");
-    printf("|        3 - Quartos               |\n");
-    printf("|        4 - Servicos              |\n");
-    printf("|        5 - Restaurante           |\n");
-    printf("|        6 - Garagem               |\n");
-    printf("|        7 - Equipe                |\n");
-    printf("|        8 - Sobre                 |\n");
-    printf("|        0 - Sair                  |\n");
-    printf("|__________________________________|\n");
-    printf("\n");
-    printf("Digite sua opcao: ");
-    scanf("%d",&opcao);
-    return opcao;
-    getchar();  
-}
 
 
 void tela_sobre(void){
---
+++ menu_inicial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu_inicial.h"
+
+int tela_inicial(void){
+    system("clear||cls");
+    int opcao;
+    printf("\n");
+    printf(" __________________________________\n");
+    printf("|                                  |\n");
+    printf("|          Menu Principal          |\n");
+    printf("|__________________________________|\n");
+    printf("|                                  |\n");
+    printf("|        1 - Clientes              |\n");
+    printf("|        2 - Funcionarios          |\n");
+    printf("|        3 - Quartos               |\n");
+    printf("|        4 - Servicos              |\n");
+    printf("|        5 - Restaurante           |\n");
+    printf("|        6 - Garagem               |\n");
+    printf("|        7 - Equipe                |\n");
+    printf("|        8 - Sobre                 |\n");
+    printf("|        0 - Sair                  |\n");
+    printf("|__________________________________|\n");
+    printf("\n");
+    printf("Digite sua opcao: ");
+    scanf("%d",&opcao);
+    return opcao;
+    getchar();  
+}
