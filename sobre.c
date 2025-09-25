02b3a215aec3473606a5634ed553779871f0760f
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-09-25 13:42:54 +0000

    Update sobre.c

--- sobre.c
+++ sobre.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sobre.h"
+#include "utilidades.h"
 
 void tela_sobre(void){
+    limpa_tela();
     system("clear||cls");
     printf("\n");
-    printf(" ____________________________________________________________________\n");
+    printf("┌────────────────────────────────────────────────────────────────────┐\n");
     printf("|                                                                    |\n");
     printf("|             UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE            |\n");
     printf("|                 CENTRO DE ENSINO SUPERIOR DO SERIDO                |\n");
@@ -18,7 +20,8 @@ void tela_sobre(void){
     printf("|     Projeto proposto pelo professor de Sistemas de Informação      |\n");
     printf("|   da UFRN-CERES, @flgorgonio, com o intuido fazer um sistema de    |\n");
     printf("|              gestão designado a hotéis ou pousadas                 |\n");
-    printf("|____________________________________________________________________|\n");
+    printf("|                                                                    |\n");
+    printf("└────────────────────────────────────────────────────────────────────┘\n");
     printf("\n");
     getchar();
 }
