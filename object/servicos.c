88d1d3421293567c352f18255c3b9f6d096cdaee
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-09-30 01:10:12 +0000

    Update servicos.c

--- object/servicos.c
+++ object/servicos.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "servicos.h"
+#include "utilidades.h"
 
 void modulo_servicos(void){
 
@@ -11,14 +12,24 @@ void modulo_servicos(void){
         op = tela_servicos();
         switch (op) {
             case '1':
-                getchar();
+                cad_servico();
+                break;
+            case '2':
+                edit_servicos();
+                break;
+            case '3':
+                list_servicos();
+                break;
+            case '4':
+                atri_servico();
                 break;
         }
     } while (op != '0'); 
 }
 
 char tela_servicos(void){
-    system("clear||cls");
+    limpa_tela();
+
     char op;
     printf("\n");
     printf("┌──────────────────────────────────────────────────────┐\n");
@@ -27,9 +38,9 @@ char tela_servicos(void){
     printf("|                                                      |\n");
     printf("|──────────────────────────────────────────────────────|\n");
     printf("|                                                      |\n");
-    printf("|       [1] -> Cadastrar Servico                       |\n");
-    printf("|       [2] -> Editar Servico                          |\n");
-    printf("|       [3] -> Listar Servicos                         |\n");
+    printf("|       [1] -> Cadastrar Serviços                      |\n");
+    printf("|       [2] -> Editar Serviços                         |\n");
+    printf("|       [3] -> Listar Serviços                         |\n");
     printf("|       [4] -> Atribuir Servico ao funcionario         |\n");
     printf("|       [0] -> Voltar                                  |\n");
     printf("└──────────────────────────────────────────────────────┘\n");
@@ -37,10 +48,73 @@ char tela_servicos(void){
     printf("Digite uma opção: ");
     scanf("%c", &op);
     getchar();
+    printf("\n");
     return op;
 }
 
 
 void cad_servico(void){
+    limpa_tela();
 
+    FILE *arq_servicos;
+    char servi[55];
+
+    printf("\n");
+    printf("┌───────────────────────────────────────────────────────┐\n");
+    printf("|#######################################################|\n");
+    printf("|#                                                     #|\n");
+    printf("|#           {Serviços -> Cadastrar Serviço}           #|\n");
+    printf("|#                                                     #|\n");
+    printf("|#######################################################|\n");
+    printf("└───────────────────────────────────────────────────────┘\n");
+    printf("\n");
+    printf("Digite o serviço:");
+    scanf("%s", servi);
+    getchar();
+    printf("\n");
+
+    arq_servicos = fopen("./data/servicos.csv", "at");
+    if(arq_servicos == NULL){
+        printf("\t Erro ao abrir o arquivo serviços\n");
+        printf("\t {Digite ENTER para continuar}\n");
+        getchar();
+        return;
+    }
+    fprintf(arq_servicos, "%s", servi);
+    fclose(arq_servicos);
+
+    limpa_tela();
+    printf("\n");
+    printf("┌────────────────────────────────────────────────────────────┐\n");
+    printf("│############################################################│\n");
+    printf("│#                                                          #│\n");
+    printf("│#                   {Serviço Cadastrado}                   #│\n");
+    printf("│#                                                          #│\n");
+    printf("│############################################################│\n");
+    printf("└────────────────────────────────────────────────────────────┘\n");
+    printf("\n");
+    printf("\nServiço Cadastrado: %s", servi);
+    printf("\n");
+    printf("\t {Digite ENTER para continuar}\n");
+    getchar();
+    printf("\n");
+
+}
+
+
+void edit_servicos(void){
+    limpa_tela();
+    return;
+}
+
+
+void list_servicos(void){
+    limpa_tela();
+    return;
+}
+
+
+void atri_servico(void){
+    limpa_tela();
+    return;
 }
