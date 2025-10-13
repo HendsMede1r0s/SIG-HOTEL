c13744395d3a7fd96435ed18bf4a6fc58e1b15da
Eriky-Rayan <eriky.medeiros.706@ufrn.edu.br>
2025-10-12 22:57:28 -0300

    refact: modulo quartos

--- include/quartos.h
+++ include/quartos.h
@@ -3,18 +3,16 @@ char tela_quartos(void);
 void check_in(void);
 void list_quartos(void);
 void check_out(void);
-void exib_quartos(void);
+void busc_quartos(void);
 void edit_quartos(void);
 void cad_quartos(void);
 
 struct quartos
 {
     char n_quarto [7];
-    char n_quarto_lido [7];
     char cpf [18];
     char quan_pessoas [5];
     char status;
-    char op_quartos;
 };
 
 typedef struct quartos Quartos;
