96ca4db6d00fa9bccee0dd9801b640d338f859e2
Eriky-Rayan <eriky.medeiros.706@ufrn.edu.br>
2025-10-22 12:22:10 -0300

    add: leitura.c

---
+++ include/leitura.h
@@ -0,0 +1,8 @@
+//////////////////
+//modulo leitura//
+//////////////////
+
+void ler_nome(char *variavel, int tam);
+void ler_cpf(char *variavel, int tam);
+void ler_cell(char *variavel, int tam);
+void ler_n_quarto(char *variavel, int tam);
