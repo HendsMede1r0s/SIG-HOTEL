4f98a201692a2d432fe22d2dbc163ae1bb2af0f6
Eriky-Rayan <eriky.medeiros.706@ufrn.edu.br>
2025-11-02 22:46:20 -0300

    fix: mensagem_erro_cpf

--- include/validacoes.h
+++ include/validacoes.h
@@ -5,7 +5,6 @@
 int valida_letra(char letra);
 int valida_digito(char num);
 int valida_num(char *num);
-char* remove_char_especial(char* texto);
 int is_numeric(const char *num);
 int valida_cpf(char *CPF_original);
 int valida_id(char *ID);
