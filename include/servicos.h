e8cce06bf1dc0a431fd0b474ed0fd2d1861cb2bc
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-11-04 00:12:51 +0000

    Update servicos.h
    coloquei o id em servicos

--- include/servicos.h
+++ include/servicos.h
@@ -1,14 +1,16 @@
+
+struct servicos{
+
+    char servi[55];
+    char id[4];
+
+};
+
+typedef struct servicos Servicos;
+
+
 void modulo_servicos(void);
 char tela_servicos(void);
 void cad_servico(void);
 void list_servicos(void);
 void exclu_servicos(void);
-
-
-struct servicos{
-
-    char servi[55];
-
-};
-
-typedef struct servicos Servicos;
