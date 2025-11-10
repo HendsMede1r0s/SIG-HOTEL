c23633e9d7b001a52c50401ae780e20d4629d621
Isaac Vilton Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-11-10 16:24:21 -0300

    feat: verifica ID existente em servicos.c

--- include/servicos.h
+++ include/servicos.h
@@ -10,6 +10,7 @@ typedef struct servicos Servicos;
 
 
 void modulo_servicos(void);
+int verifica_id(const char *id_a_verificar);
 char tela_servicos(void);
 void cad_servico(void);
 void list_servicos(void);
